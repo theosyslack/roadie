45166f1dc1e136ac521d8128de61575771179302
Theo Syslack <theosyslack@gmail.com>
2020-03-14 13:50:52 -0500

    Enable cors

--- server/src/server/index.ts
+++ server/src/server/index.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import home from './routes/home';
 import facilities from './routes/facilities';
 import logServerStart from '../log/logServerStart';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
 export default function startServer () {
+  // Allow Cross-Origin Requests
+  app.use(cors());
+
   // Add Routes
   home(app);
   facilities(app);
