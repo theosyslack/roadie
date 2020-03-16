a6fd7b18294f01d1d505b0cfe451d84b96f8d246
Theo Syslack <tsyslack@crescendocollective.com>
2020-03-16 18:55:01 -0500

    Update build to _not_ execute docker-compose up immediately

--- build.sh
+++ build.sh
@@ -2,26 +2,25 @@
 export CLIENT_PORT=80
 export SERVER_PORT=3030
 # API_KEY should be set externally, so it's not traked by git.
-echo "Now run `docker-compose up` Your site will be visible at http://localhost:${CLIENT_PORT}"
 if [ -z $API_KEY ]; then
   echo "Please set your API_KEY before continuing."
 else 
   echo "Building 🏕️ Roadie"
   echo "Building Client..."
   cd client
-  yarn 
+  yarn --silent
   yarn build
 
   echo "Building Server..."
   cd ../server
-  yarn
+  yarn --silent
   yarn build
 
   echo "Building Container..."
   cd ../
-  docker-compose build
+  docker-compose --log-level ERROR build
 
   echo "Completed 🏕️ Roadie"
-  echo "Now run docker-compose up."
-  echo "Your site will be visible at http://localhost:${CLIENT_PORT}".
+  echo "Now run 'docker-compose up'"
+  echo "🏕️ Roadie will be visible at http://localhost:${CLIENT_PORT}"
 fi
