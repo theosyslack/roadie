62bf5c7e034e4189442c958eabec490562c0326d
Theo Syslack <tsyslack@crescendocollective.com>
2020-03-16 18:44:58 -0500

    Add more explict build message

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 export CLIENT_PORT=80
 export SERVER_PORT=3030
 # API_KEY should be set externally, so it's not traked by git.
-
+echo "Now run `docker-compose up` Your site will be visible at http://localhost:${CLIENT_PORT}"
 if [ -z $API_KEY ]; then
   echo "Please set your API_KEY before continuing."
 else 
@@ -22,5 +22,6 @@ else
   docker-compose build
 
   echo "Completed 🏕️ Roadie"
-  echo "Now run `docker-compose up`"
+  echo "Now run docker-compose up."
+  echo "Your site will be visible at http://localhost:${CLIENT_PORT}".
 fi
