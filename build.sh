36a3c9e532d3d697b493fca045b7854e00b38ed2
Theo Syslack <tsyslack@crescendocollective.com>
2020-03-16 22:05:15 -0500

    update README with streamlined setup

--- build.sh
+++ build.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
-export CLIENT_PORT=80
-export SERVER_PORT=3030
-# API_KEY should be set externally, so it's not traked by git.
-if [ -z $API_KEY ]; then
-  echo "Please set your API_KEY before continuing."
-else 
-  echo "Building 🏕️ Roadie"
-  echo "Building Client..."
-  cd client
-  yarn --silent
-  yarn build
+source .env
 
-  echo "Building Server..."
-  cd ../server
-  yarn --silent
-  yarn build
+echo "Building 🏕️ Roadie"
+echo "Building Client..."
+cd client
+yarn --silent
+yarn build
 
-  echo "Building Container..."
-  cd ../
-  docker-compose --log-level ERROR build
+echo "Building Server..."
+cd ../server
+yarn --silent
+yarn build
 
-  echo "Completed 🏕️ Roadie"
-  echo "Now run 'docker-compose up'"
-  echo "🏕️ Roadie will be visible at http://localhost:${CLIENT_PORT}"
-fi
+echo "Building Container..."
+cd ../
+docker-compose config
+docker-compose build
+
+echo "Completed 🏕️ Roadie"
+echo "Now run 'docker-compose up'"
+echo "🏕️ Roadie will be visible at http://localhost:${CLIENT_PORT}"
+exit
